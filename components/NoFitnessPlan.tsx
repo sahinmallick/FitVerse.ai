626b1c04336114f2dbea23ae07f7fbfc01abc000
sahinmallick14 <sahinmallick14>
2025-06-28 23:25:04 +0530

    updating for first time

---
+++ components/NoFitnessPlan.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NoFitnessPlan = () => {
+  return <div>No fitness plan</div>;
+};
+
+export default NoFitnessPlan;
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://popular-monitor-57.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
