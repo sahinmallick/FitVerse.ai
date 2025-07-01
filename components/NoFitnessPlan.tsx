55eb589cc6f576027dfbd41e7bf8404ac5ec4487
rimanshupatel <rimanshupatel1@gmail.com>
2025-07-01 15:04:44 +0530

    Cleared

--- components/NoFitnessPlan.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NoFitnessPlan = () => {
-  return <div>No fitness plan</div>;
-};
-
-export default NoFitnessPlan;
--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  providers: [
-    {
-      domain: "https://popular-monitor-57.clerk.accounts.dev/",
-      applicationID: "convex",
-    },
-  ],
-};
